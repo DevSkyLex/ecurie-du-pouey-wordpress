7b186fc00d7df30579d6a272c469aa4dca3b5956
Amaury Franssen <amauryfranssen@gmail.com>
2024-10-15 11:34:20 +0200

    config:set up with good prefix

--- wp-config.php
+++ wp-config.php
@@ -1,9 +1,90 @@
+johan.dupraz@ynov.com
+
+noemie.lusoki@gmail.com
+
+elevage-du-pouey.com
+
+first lady du pouey : nom du cheval avec affixe
+
+famille, soin, 
+
+amateur et qq pro
+
+notoriété et vendre des chevaux, c plus pour vendre
+
+les propriétaire qui von t revendre c plus ça la concurrence dur pour s'en débarasser à sous-prix
+
+noir or
+
+Ecurie du Pouey entre Pau et Toulouse dans le 65 20 ans Ancienneté 2 enfants de 6 et 2 ans Marie salarié depuis juin 37 ans + Stagiaires de lycée professionnel Elevage de chevaux selle français et anglo arabe de sport = CSO et Complet Anglais ara
+
+
+Cheffe de projet : Mathilde 
+
+Ecurie du Pouey entre Pau et Toulouse dans le 65
+20 ans Ancienneté 
+2 enfants de 6 et 2 ans 
+Marie salarié depuis juin 37 ans 
++ Stagiaires de lycée professionnel 
+Elevage de chevaux selle français et anglo arabe de sport = CSO et Complet
+Anglais arabe et pur sang = courses
+Valorisation du cheval 
+Inséminatrice + jument de l'extérieur 
+Vente 
+CSO + Concours complet
+Ecurie proprietaire une dizaine = certains depuis 20 ans 
+Propriété de 70hectares 
+Alimentation fait par eux-mêmes 
+Début de ventes à l'étranger
+Communiquer sur leurs chevaux, les résultats concours et les choix de croisement + gagner en visibilité 
+Page Facebook + Compte insta : Stéphanie .. 
+( proposition d'un outil de gestion des réseaux sociaux?) 
+Vidéos, tutos commerciaux 
+But : Communiquer quotidiennement 
+80 chevaux ! 
+30-35 à eux 
+Valeurs : le respect du cheval ; chevaux facile d'utilisation et client satisfait, cheval qui dure sur le long terme; pas d'exploitation des 4 ans, ils prennent le temps 
+Exploitation familiale, entre 2 et 6 poulains par an 
+Rapport de proximité avec les clients + Qualité de vie des chevaux 
+entre 20 et 30k prix de départ pour chevaux
+budget de communication : aucune idée mais petit budget 
+Fiche technique d'un cheval ??
+Nom de domaine : élevage du Pouey 
+Concurrence : tous les autres éleveurs + des particuliers qui font du pseudo élevage et vendent moins chèr 
+Stratégie pour : + notoriété + visibilité pour la vente 
+Formulaire de contact : téléphone ou mail ou réseaux sociaux donc call to action + localisation 
+Immersion 3D sur site ?? 
+Vente bouche à oreille et réseaux sociaux comme Facebook 
+Ecurie du Pouey devient une référence 
+Vente à l'étranger : Quatar , Europe, Etats Unis, Royaume-Uni, Corse, Italie ou Sardeigne 
+Communication Français peut-être Anglais?? 
+Couleurs : logo noir ou bleu et de l'or, logo abstrait trait d'un cote tete de cheval imbriqué avec poulain 
+Un cote sport un cote elevage 
+Jeu de montagne derrière? 
+Site du haras de maleray cote bordeaux site de chevaux de dressage 
+
+g3nFCY@j2LLmzQOPlI4mM6pL
+
+bynet
+2])erD6nxT.5"bx
+f=infinity
+admin.pouey
+%uYfEUG$X8
+
+https://miro.com/app/board/uXjVLTW0lLc=/
+
+
+mahieu.manon12@gmail.com
+
+06 41 14 14 14 mathilde (responsable de groupe)
+
+
 <?php
 /**
  * The base configuration for WordPress
  *
  * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the website, you can copy this file to "wp-config.php"
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
  * and fill in the values.
  *
  * This file contains the following configurations:
@@ -11,31 +92,13 @@
  * * Database settings
  * * Secret keys
  * * Database table prefix
+ * * Localized language
  * * ABSPATH
  *
- * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
  *
  * @package WordPress
  */
-
-// ** Database settings - You can get this info from your web host ** //
-// /** The name of the database for WordPress */
-// define( 'DB_NAME', 'if0_37512536_wp86155' );
-
-// /** Database username */
-// define( 'DB_USER', '37512536_5' );
-
-// /** Database password */
-// define( 'DB_PASSWORD', 'p8l[Sj906@' );
-
-// /** Database hostname */
-// define( 'DB_HOST', 'sql206.byetcluster.com' );
-
-// /** Database charset to use in creating database tables. */
-// define( 'DB_CHARSET', 'utf8mb4' );
-
-/** The database collate type. Don't change this if in doubt. */
-// define( 'DB_COLLATE', '' );
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
@@ -67,14 +130,16 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'qu7jm26iekryhbc08ccimpagmsitboml7iasxlwyq8omuoxficmands8u4ao1nld' );
-define( 'SECURE_AUTH_KEY',  'q63co8gw3md9qhakqapwywwta98hlhafqbzq8oxhduadtsibd1mocwqdmqsvmonp' );
-define( 'LOGGED_IN_KEY',    'dprwdkc1yulnxq1al4dcoisptu7iqheizqvshabbi2jzvwu53dwuq7hdlp70zt6h' );
-define( 'NONCE_KEY',        'vkn540tdrpgfvs5l9zbejc4n0kjlvc8y7uiha1bzhnxqlbymazm9yp0xgw2ivuks' );
-define( 'AUTH_SALT',        'n0iipyis7kptqkbsmsvbv533sx1klxmcfsyvwu3phuzxaueupf4lcpr0as2otxzg' );
-define( 'SECURE_AUTH_SALT', 'ackqwbfjj0xtmvjwz3ltluehnn2tzypqphrrqlutp4qhxwfcbm8p3er3xa0wczis' );
-define( 'LOGGED_IN_SALT',   'k7k6jmbdoletmj6upfqhdpzhpdumcqgkpmfthkmk0lxkxmurncsfzlfwne5byvjf' );
-define( 'NONCE_SALT',       '6vfbt5akkxiek0hj0s4fz8jcdosds3pkjm0ku3gqzuevaxqymbpfa7261rjyipkd' );
+define( 'AUTH_KEY',          'Ze]pF`5t<?2+MUR{RUt$}LkGsQL%*q)9ur9H(B7NZ46U=6gPO=F*9WAHD]lEK<:~' );
+define( 'SECURE_AUTH_KEY',   '0++wDk>CEz)x+OGmWgHTU[hyA|HYv&Ju!GtA5YGiAug:Tdyl9SOWNp*:mk[<|?Nv' );
+define( 'LOGGED_IN_KEY',     'ej1js%EU:If.jEW{*k/HWkc=XX5wn; {F; Wt nreYq3bQt{v`xmmCzn[mg,S7=G' );
+define( 'NONCE_KEY',         '2I?zE)|~E_zAJ}i%<RN+xW0%6v8Q+.cU;W0L^(/>9SZcb@3!hwm~=f=g<v:swip)' );
+define( 'AUTH_SALT',         'mVJ41^%9#EdABkS87Qa%:u:%DE-O(`t^j96gi)#T]hk!>m[.z/)@}*;7cUi1,bk;' );
+define( 'SECURE_AUTH_SALT',  '([EjonD}t1fQzur]e+%MVQSH2c&pqjN%ruv~FR vLVjZ-XbTUx&@`9O49a2lkah+' );
+define( 'LOGGED_IN_SALT',    'y]i-yhlW6Ts0;-rtc}<gXs5be@&vt=E,5oq|7zq+<C]blXz@G$]EKX;b;3e[p} :' );
+define( 'NONCE_SALT',        'WJ^o[zjxy!<+QoSX7V^c+S4f|oW]WfLE&`)Zq}kgG++!JAIfbUsU>77]$$`(:d$q' );
+define( 'WP_CACHE_KEY_SALT', 'z^&]IOc<W<)7Gv!_=d~M(8HqyVu.Z]`3w@NUB%Pq7pHOm7~C=X55_@fYs11n8s9<' );
+
 
 /**#@-*/
 
@@ -84,7 +149,12 @@ define( 'NONCE_SALT',       '6vfbt5akkxiek0hj0s4fz8jcdosds3pkjm0ku3gqzuevaxqymbp
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix = 'wpu3_';
+$table_prefix = 'wp_';
+
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
 
 /**
  * For developers: WordPress debugging mode.
@@ -96,14 +166,13 @@ $table_prefix = 'wpu3_';
  * For information on other constants that can be used for debugging,
  * visit the documentation.
  *
- * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+if ( ! defined( 'WP_DEBUG' ) ) {
+	define( 'WP_DEBUG', false );
+}
 
-/* Add any custom values between this line and the "stop editing" line. */
-
-
-
+define( 'WP_ENVIRONMENT_TYPE', 'local' );
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
