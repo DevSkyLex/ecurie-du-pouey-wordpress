abfa0371230ea05ba30bf0d2efd2698afce478e8
Amaury Franssen <amauryfranssen@gmail.com>
2024-10-15 18:41:41 +0200

    feat(theme): registering elementor custom widget in theme

---
+++ wp-content/themes/hello-elementor-child/custom-elementor.php
@@ -0,0 +1,45 @@
+<?php
+namespace WPC;
+
+class Widget_Loader{
+
+  private static $_instance = null;
+
+  public static function instance()
+  {
+    if (is_null(self::$_instance)) {
+      self::$_instance = new self();
+    }
+    return self::$_instance;
+  }
+
+
+  private function include_widgets_files(){
+    require_once(__DIR__ . '/elementor-widgets/advertisement.php');
+  }
+
+  public function register_widgets(){
+
+    $this->include_widgets_files();
+
+    \Elementor\Plugin::instance()->widgets_manager->register(new Widgets\Advertisement() );
+  }
+
+  public function __construct(){
+    add_action('elementor/widgets/widgets_registered', [$this, 'register_widgets'], 99);
+  }
+}
+
+// Instantiate Plugin Class
+Widget_Loader::instance();
+
+// function register_horses_widget( $widgets_manager ) {
+//     if ( class_exists( 'Elementor\Horses_Widget' ) ) {
+//         require_once( __DIR__ . '/elementor-widgets/horses-widget.php' );
+//         $widgets_manager->register( new Horses_Widget() );
+//     } else {
+//         error_log( 'Elementor\Horses_Widget class does not exist' );
+//     }
+// }
+
+// add_action( 'elementor/widgets/register', 'register_horses_widget' );
